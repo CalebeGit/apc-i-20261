26d2df09f92f78a36b6db07d5a3fe0a81d6327b8
CalebeGit <calebeasreis@gmail.com>
2026-03-18 10:59:24 -0300

    codigos da aula02

---
+++ aulas/aula02/saida.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    //1. entrada
+
+    //2. processamento
+    
+    //3. saida
+    printf("um texto qualquer\n");
+    printf("programar em C eh top!\n");
+    printf("%i\n"), 10);
+    printf("%F")
+    return 0;
+}
