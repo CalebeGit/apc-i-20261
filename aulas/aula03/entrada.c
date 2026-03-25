02e432ce5db3db5153b0bdfc87168cf1f526cc57
CalebeGit <calebeasreis@gmail.com>
2026-03-25 10:17:14 -0300

    codigos da aula03

---
+++ aulas/aula03/entrada.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    /*entrada de dados */
+
+    char tecla_pressionada;
+
+    printf("pressione uma tecla: ");
+    scanf("%c", &tecla_pressionada);
+    while(getchar() != '\n'); //limpar o buffer
+
+
+    printf("Voce pressionou a tecla %c\n", tecla_pressionada);
+
+    int idade;
+
+    printf("informe a sua idade: ");
+    scanf("%i", &idade);
+    while(getchar() != '\n'); //limpar o buffer
+
+    printf("voce tem %i de prisão\n", idade);
+
+    float preco;
+    printf("informe o preco da passagem: ");
+    scanf ("%f", &preco);
+    while(getchar() != '\n'); // limpar o buffer
+
+    printf("O preco da passagem eh %.2f\n", preco);
+
+    return 0;
+}
