428ec3a720f46a4efcd538c5e933d85f40eaf65b
CalebeGit <calebeasreis@gmail.com>
2026-04-15 10:47:50 -0300

    codigos da pratica02

---
+++ praticas/pratica02/imprima_caracteres.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    char tecla_pressionada = 'A';
+    printf("%c\n", tecla_pressionada);
+   
+    char str = 'Isso aqui eh uma string';
+
+    printf("%s\n", str);
+
+    return 0;
+
+}
---
+++ praticas/pratica02/imprima_decimais.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+   float decimal float decimal = 5.2f;
+    return 0;
+}
